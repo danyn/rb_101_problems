24ebae1d2ed71ca4e29caebb2f1162fe1b0de245
Daniel Eisen <eisen.dan@gmail.com>
2021-06-18 17:28:34 -0700

    1000 lights refactor a

--- medium_1/4/1000_lights.rb
+++ medium_1/4/1000_lights.rb
@@ -1,16 +1,12 @@
 def lights (bank_size)
   bank = Array.new(bank_size, true)
+  1.upto(bank_size) do |n|
+    step = n + 1
+    iteration_size = bank_size - n
+    iteration_size.times { |i| bank[n + i] = !bank[n + i] if i % step == 0 }
+  end
   synopsis = []
-  (bank_size - 1).times do |n|
-    start = n + 1
-    step = start + 1
-    iteration_size = bank_size - start
-    iteration_size.times { |i| bank[start + i] = !bank[start + i] if i % step == 0 }
-  end
-  bank.each_with_index do |bool_on, i|
-    synopsis << (i + 1) if bool_on
-  end
-  synopsis
+  bank.each_with_index{|bool_on, i| synopsis << (i + 1) if bool_on } 
 end
 
 p [1, 4]
@@ -21,4 +17,4 @@ p [1, 4, 9]
 p lights(10)
 p [1, 4, 9] == lights(10)
 
- # puts "start #{start} iteration_size #{iteration_size}"
+ # puts "n #{n} iteration_size #{iteration_size}"
